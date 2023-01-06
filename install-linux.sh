1b0921ddf71d4efbbc2b79d16ac4d53d03b59645
0NotAppicable0 <na.intj@pm.me>
2023-01-05 23:00:19 -0500

    feat: updated install-linux.sh

--- install-linux.sh
+++ install-linux.sh
@@ -11,11 +11,21 @@ fi
 echo "== Updating and upgrading =="
 apt-get update
 apt-get upgrade
+apt-get install unzip
 
+# Install Hack Font #
+curl -sL https://github.com/source-foundry/Hack/releases/download/v3.003/Hack-v3.003-ttf.zip
+unzip Hack-v3.003-ttf.zip
+mv Hack-v3.003-ttf /usr/share/fonts/
+fc-cache -f -v
 
-# Install ZSH #
+# Install Fish & Dependencies #
 echo "== Installing FISH =="
 apt-get install fish
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+fisher install jethrokuan/z
+apt install exa
+apt install peco
 
 # Install Other Dependencies #
 echo "== Installing stow =="
@@ -25,12 +35,23 @@ apt-get install build-essential
 apt-get install g++ gdb
 apt-get install clang++-3.8 lldb
 apt-get install gcc-multilib g++-multilib
+echo "== Installing Node =="
+apt install node
+
+# Install Neovim & Dependencies #
+apt install neovim
+apt install lua-language-server
+npm install -g typescript-language-server typescript
+npm install -g @fsouza/prettierd
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 echo "
 =======================================================================================
 Additonal notes: 
 \n 1. If you are on WSL you can type 'code' to install vscode-server
-\n 2. After install, make sure you change your default shell
+\n 2. After install, make sure you set your default shell 
 \n 3. Then reboot your shell to see reflected changes
+\n 4. To finish install of nvim ensure you start nvim and run :PackerInstall and :MasonInstall eslint_d
 =======================================================================================
 "
