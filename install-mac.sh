557b0ad7e949b670a28157b7f4f658b14f933841
na.intj@pm.me <na.intj@pm.me>
2023-11-14 15:55:09 -0500

    fix: stow of .config files

--- install-mac.sh
+++ install-mac.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 # Helper Functions Setup #
 brew_install() {
-    echo "\nInstalling $1"
+    echo "Installing $1"
     if brew list $1 &>/dev/null; then
         echo "${1} is already installed"
     else
@@ -10,30 +12,36 @@ brew_install() {
 ##########################
 
 # Sudo Check
-if [ $EUID -ne 1 ]; then
+if [ $EUID == 0 ]; then
    echo "Please do not run this script as root!" 
    exit 1
 fi
 
 # Install Package Manager (BREW)
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-eval "$(homebrew/bin/brew shellenv)"
+which -s brew
+if [[ $? != 0 ]] ; then
+    echo "Homebrew not installed, intalling..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Homebrew already installed, updating instead..."
+    brew update
+fi
 
 # Install Packages
-brew_install install stow
-brew_install install peco
-brew_install install neovim
+brew_install stow
+brew_install peco
+brew_install neovim
 
 # NVM SETUP
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 
 # ZSH SETUP
-brew_install install zsh
+brew_install zsh
 cp zsh/custom/resources/lambda-zsh-theme/cdimascio-lambda.zsh-theme zsh/custom/themes/cdimascio-lambda.zsh-theme
 cp zsh/custom/resources/spaceship-prompt/spaceship.zsh-theme zsh/custom/themes/spaceship.zsh-theme
 
 # FISH SETUP
-brew_install install fish
+brew_install fish
 
 # Stow Configurations
 stow .config
